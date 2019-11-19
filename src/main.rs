4592480fafd5ff1a38dcea29a339bf032b2d0f74
wenewzhang <wenewboy@gmail.com>
2019-11-19 16:22:05 +0800

    largest char & int

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,47 @@
-use std::fs::File;
-use std::io::ErrorKind;
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    // or
+    // for item in list.iter() {
+    //     if *item > largest {
+    //         largest = *item;
+    //     }
+    // }
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 
 fn main() {
-    let f = File::open("hello2.txt").map_err(|error| {
-        if error.kind() == ErrorKind::NotFound {
-            File::create("hello.txt").unwrap_or_else(|error| {
-                panic!("Tried to create file but there was a problem: {:?}", error);
-            })
-        } else {
-            panic!("There was a problem opening the file: {:?}", error);
-        }
-    });
-    // if let Err(e) = writeln!(f, "A new line!") {
-    //     eprintln!("Couldn't write to file: {}", e);
-    // }
+    let number_list = vec![34, 50, 25, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }

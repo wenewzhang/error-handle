3241039a0d5421e2e4dffca0d5e88e714deefa44
wenewzhang <wenewboy@gmail.com>
2019-11-19 20:13:54 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,9 @@
-fn largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    // or
-    // for item in list.iter() {
-    //     if *item > largest {
-    //         largest = *item;
-    //     }
-    // }
-    largest
+struct Point<T, U> {
+    x: T,
+    y: U,
 }
-
-fn largest_char(list: &[char]) -> char {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    largest
-}
-
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
-    let char_list = vec!['y', 'm', 'a', 'q'];
-
-    let result = largest_char(&char_list);
-    println!("The largest char is {}", result);
+    let integer = Point { x: 5, y: 10 };
+    let float = Point { x: 1.0, y: 4.0 };
 }

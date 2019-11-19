910e701d5409a27133d29f118de7ed43ee9bccf4
wenewzhang <wenewboy@gmail.com>
2019-11-19 15:21:43 +0800

    append

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
-use std::fs::File;
-use std::io::ErrorKind;
+use std::fs::OpenOptions;
 use std::io::prelude::*;
 
 fn main() {
-    let fp = File::open("hello.txt");
-    let mut f = match fp {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(fc) => fc,
-                Err(e) => panic!("Tried to create file but there was a problem: {:?}", e),
-            },
-            other_error => panic!("There was a problem opening the file: {:?}", other_error),
-        },
-    };
-    f.write_all(b"Hello, world!");
-    f.sync_all();
+    let mut file = OpenOptions::new()
+        .write(true)
+        .append(true)
+        .create(true)
+        .open("helloworld.txt")
+        .unwrap();
+
+    if let Err(e) = writeln!(file, "A new line!") {
+        eprintln!("Couldn't write to file: {}", e);
+    }
 }

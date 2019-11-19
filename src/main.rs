9187b68e605fd48fff716edaa68e8f773c6b3487
wenewzhang <wenewboy@gmail.com>
2019-11-19 16:38:58 +0800

    PartialOrd & Copy trait

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,4 @@
-fn largest(list: &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item > largest {
-            largest = item;
-        }
-    }
-
-    // or
-    // for item in list.iter() {
-    //     if *item > largest {
-    //         largest = *item;
-    //     }
-    // }
-    largest
-}
-
-fn largest_char(list: &[char]) -> char {
+fn largest<T:PartialOrd + Copy>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list.iter() {
@@ -27,7 +9,6 @@ fn largest_char(list: &[char]) -> char {
 
     largest
 }
-
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
@@ -35,13 +16,8 @@ fn main() {
     let result = largest(&number_list);
     println!("The largest number is {}", result);
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-
     let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
+    let result = largest(&char_list);
     println!("The largest char is {}", result);
 }

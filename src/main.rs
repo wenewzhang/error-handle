75043a6274f738e2e0f8b3683346665a052a6e9c
wenewzhang <wenewboy@gmail.com>
2019-11-20 15:35:51 +0800

    error

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,14 @@
-
-
 fn main() {
-    let r;
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    {
-        let x = 5;
-        r = &x;
-    }
-
-    println!("r: {}", r);
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {}", result);
 }
-
-// {
-//     let x = 5;            // ----------+-- 'b
-//                           //           |
-//     let r = &x;           // --+-- 'a  |
-//                           //   |       |
-//     println!("r: {}", r); //   |       |
-//                           // --+       |
-// }                         // ----------+
-//
-// Listing 10-19: A valid reference because the data has a longer lifetime than the reference
+fn longest(x: &str, y: &str) -> &str {
+    if x.len() > y.len() {
+        &x
+    } else {
+        &y
+    }
+}

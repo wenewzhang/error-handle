5c4c50ab8d5cb191bd7632d1a4906ea481e0305f
wenewzhang <wenewboy@gmail.com>
2019-11-20 16:15:58 +0800

    annotation of reference

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,10 @@ fn main() {
     let result = longest(string1.as_str(), string2);
     println!("The longest string is {}", result);
 }
-fn longest(x: &str, y: &str) -> &str {
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
-        &x
+        x
     } else {
-        &y
+        y
     }
 }

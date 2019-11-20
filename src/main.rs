abd018799c1fd2a271b98f89f9638d930d7726d7
wenewzhang <wenewboy@gmail.com>
2019-11-20 17:01:19 +0800

    print struct with reference annotation

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,22 @@
-// fn main() {
-//     let string1 = String::from("abcd");
-//     let string2 = "xyz";
-//
-//     let result = longest(string1.as_str(), string2);
-//     println!("The longest string is {}", result);
-// }
+use std::fmt;
 
-// fn main() {
-//     let string1 = String::from("long string is long");
-//
-//     {
-//         let string2 = String::from("xyz");
-//         let result = longest(string1.as_str(), string2.as_str());
-//         println!("The longest string is {}", result);
-//     }
-// }
-
-//error happen example
-fn main() {
-    let string1 = String::from("long string is long");
-    let result;
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-    }
-    println!("The longest string is {}", result);
+#[derive(Debug)]
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
-//
-// error[E0597]: `string2` does not live long enough
-//   --> src/main.rs:26:44
-//    |
-// 26 |         result = longest(string1.as_str(), string2.as_str());
-//    |                                            ^^^^^^^ borrowed value does not live long enough
-// 27 |     }
-//    |     - `string2` dropped here while still borrowed
-// 28 |     println!("The longest string is {}", result);
-//    |                                          ------ borrow later used here
 
-fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
+impl<'a>  fmt::Display for ImportantExcerpt<'a>  {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // Customize so only `x` and `y` are denoted.
+        write!(f, "{}", self.part)
     }
+}
+
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let i = ImportantExcerpt { part: first_sentence };
+    println!("{}",i)
 }

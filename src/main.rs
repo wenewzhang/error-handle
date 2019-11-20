9b795105bb0b0a42d7422d516ff74ec367ca9a9f
wenewzhang <wenewboy@gmail.com>
2019-11-20 10:45:08 +0800

    mix

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,20 @@ struct Point<T, U> {
     y: U,
 }
 
+impl<T, U> Point<T, U> {
+    fn mixup<V, Z>(self, other: Point<V, Z>) -> Point<T, Z> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
+    }
+}
+
 fn main() {
-    let integer = Point { x: 5, y: 10 };
-    let float = Point { x: 1.0, y: 4.0 };
+    let p1 = Point { x: 5, y: 10.4 };
+    let p2 = Point { x: "Hello", y: 'c'};
+
+    let p3 = p1.mixup(p2);
+
+    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
 }

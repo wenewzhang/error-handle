49fe830e6e0bfb0b1c3a9ba96991cc41c9b33e8b
wenewzhang <wenewboy@gmail.com>
2019-11-21 16:58:46 +0800

    deref

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,27 @@
-use std::thread;
-use std::time::Duration;
+use std::ops::Deref;
 
-fn simulated_expensive_calculation(intensity: u32) -> u32 {
-    println!("calculating slowly...");
-    thread::sleep(Duration::from_secs(2));
-    intensity
-}
+impl<T> Deref for MyBox<T> {
+    type Target = T;
 
-fn generate_workout(intensity: u32, random_number: u32) {
-    let expensive_closure = |num| {
-        println!("calculating slowly...");
-        thread::sleep(Duration::from_secs(2));
-        num
-    };
-
-    if intensity < 25 {
-        println!(
-            "Today, do {} pushups!",
-            expensive_closure(intensity)
-        );
-        println!(
-            "Next, do {} situps!",
-            expensive_closure(intensity)
-        );
-    } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay hydrated!");
-        } else {
-            println!(
-                "Today, run for {} minutes!",
-                expensive_closure(intensity)
-            );
-        }
+    fn deref(&self) -> &T {
+        &self.0
     }
 }
-// or
-// fn generate_workout(intensity: u32, random_number: u32) {
-//     let expensive_result =
-//         simulated_expensive_calculation(intensity);
-//
-//     if intensity < 25 {
-//         println!(
-//             "Today, do {} pushups!",
-//             expensive_result
-//         );
-//         println!(
-//             "Next, do {} situps!",
-//             expensive_result
-//         );
-//     } else {
-//         if random_number == 3 {
-//             println!("Take a break today! Remember to stay hydrated!");
-//         } else {
-//             println!(
-//                 "Today, run for {} minutes!",
-//                 expensive_result
-//             );
-//         }
-//     }
-// }
 
-// fn generate_workout(intensity: u32, random_number: u32) {
-//     if intensity < 25 {
-//         println!(
-//             "Today, do {} pushups!",
-//             simulated_expensive_calculation(intensity)
-//         );
-//         println!(
-//             "Next, do {} situps!",
-//             simulated_expensive_calculation(intensity)
-//         );
-//     } else {
-//         if random_number == 3 {
-//             println!("Take a break today! Remember to stay hydrated!");
-//         } else {
-//             println!(
-//                 "Today, run for {} minutes!",
-//                 simulated_expensive_calculation(intensity)
-//             );
-//         }
-//     }
-// }
+
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
 
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+    let x = 5;
+    let y = Box::new(x);
 
-    generate_workout(
-        simulated_user_specified_value,
-        simulated_random_number
-    );
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 }

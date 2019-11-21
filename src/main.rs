e83340216c2b63c92b37f222c0eaf321d2f59376
wenewzhang <wenewboy@gmail.com>
2019-11-21 15:29:00 +0800

    comment

--- src/main.rs
+++ src/main.rs
@@ -20,3 +20,10 @@ fn main() {
     let i = ImportantExcerpt { part: first_sentence };
     println!("{}",i)
 }
+
+//Elision rulers
+// The first rule is that each parameter that is a reference gets its own lifetime parameter. In other words, a function with one parameter gets one lifetime parameter: fn foo<'a>(x: &'a i32); a function with two parameters gets two separate lifetime parameters: fn foo<'a, 'b>(x: &'a i32, y: &'b i32); and so on.
+//
+// The second rule is if there is exactly one input lifetime parameter, that lifetime is assigned to all output lifetime parameters: fn foo<'a>(x: &'a i32) -> &'a i32.
+//
+// The third rule is if there are multiple input lifetime parameters, but one of them is &self or &mut self because this is a method, the lifetime of self is assigned to all output lifetime parameters.

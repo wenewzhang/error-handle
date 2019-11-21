dddc93d06621b7594e3acac2c727b6d3d0b42912
wenewzhang <wenewboy@gmail.com>
2019-11-21 16:08:15 +0800

    error

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
-use std::fmt;
+use std::env;
+use std::process;
 
-#[derive(Debug)]
-struct ImportantExcerpt<'a> {
-    part: &'a str,
-}
+fn main() {
+    let args: Vec<String> = env::args().collect();
 
-impl<'a>  fmt::Display for ImportantExcerpt<'a>  {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Customize so only `x` and `y` are denoted.
-        write!(f, "{}", self.part)
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
+
+        process::exit(1);
     }
 }
 
-fn main() {
-    let novel = String::from("Call me Ishmael. Some years ago...");
-    let first_sentence = novel.split('.')
-        .next()
-        .expect("Could not find a '.'");
-    let i = ImportantExcerpt { part: first_sentence };
-    println!("{}",i)
-}
-
-//Elision rulers
-// The first rule is that each parameter that is a reference gets its own lifetime parameter. In other words, a function with one parameter gets one lifetime parameter: fn foo<'a>(x: &'a i32); a function with two parameters gets two separate lifetime parameters: fn foo<'a, 'b>(x: &'a i32, y: &'b i32); and so on.
+// fn main() {
+//     let args: Vec<String> = env::args().collect();
+//     println!("{:?}", args);
+//     // let (query, filename) = parse_config(&args);
 //
-// The second rule is if there is exactly one input lifetime parameter, that lifetime is assigned to all output lifetime parameters: fn foo<'a>(x: &'a i32) -> &'a i32.
+//     // --snip--
+// }
 //
-// The third rule is if there are multiple input lifetime parameters, but one of them is &self or &mut self because this is a method, the lifetime of self is assigned to all output lifetime parameters.
+// fn parse_config(args: &[String]) -> (&str, &str) {
+//     let query = &args[1];
+//     let filename = &args[2];
+//
+//     (query, filename)
+// }

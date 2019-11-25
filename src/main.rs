fb9c816f71f75b424cd4e4fbb8e6d480437bf4c9
wenewzhang <wenewboy@gmail.com>
2019-11-25 17:01:37 +0800

    args

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,36 @@
-use std::ops::Deref;
-
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &T {
-        &self.0
-    }
-}
-
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
-}
-
+use std::env;
+use std::fs;
 
 fn main() {
-    let x = 5;
-    let y = Box::new(x);
+    let args: Vec<String> = env::args().collect();
 
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
+    let config = parse_config(&args);
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    let contents = fs::read_to_string(config.filename)
+        .expect("Something went wrong reading the file");
+
+    // --snip--
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Config { query, filename }
+    }
+}
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }

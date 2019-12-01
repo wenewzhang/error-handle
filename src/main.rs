e90d1ea4b421a20ab9c2f9432035a356b55d0de8
wenewzhang <wenewboy@gmail.com>
2019-12-01 12:06:25 +0800

    rx/tx

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 use std::thread;
-use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
-     let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {} from the spawned thread!", i);
-            thread::sleep(Duration::from_millis(1));
-        }
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let val = String::from("hi");
+        tx.send(val).unwrap();
     });
 
-    for i in 1..5 {
-        println!("hi number {} from the main thread!", i);
-        thread::sleep(Duration::from_millis(1));
-    }
-    handle.join().unwrap();
+    let received = rx.recv().unwrap();
+    println!("Got: {}", received);
 }

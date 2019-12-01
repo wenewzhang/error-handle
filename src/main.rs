f517403c9bbd37b07d2f2dfa39719a36e8c4e10f
wenewzhang <wenewboy@gmail.com>
2019-12-01 11:58:45 +0800

    animal & dog

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,22 @@
-trait Pilot {
-    fn fly(&self);
+trait Animal {
+    fn baby_name() -> String;
 }
 
-trait Wizard {
-    fn fly(&self);
-}
+struct Dog;
 
-struct Human;
-
-impl Pilot for Human {
-    fn fly(&self) {
-        println!("This is your captain speaking.");
+impl Dog {
+    fn baby_name() -> String {
+        String::from("Spot")
     }
 }
 
-impl Wizard for Human {
-    fn fly(&self) {
-        println!("Up!");
+impl Animal for Dog {
+    fn baby_name() -> String {
+        String::from("puppy")
     }
 }
-
-impl Human {
-    fn fly(&self) {
-        println!("*waving arms furiously*");
-    }
-}
-// 
-// fn main() {
-//     let person = Human;
-//     person.fly();
-// }
 
 fn main() {
-    let person = Human;
-    Pilot::fly(&person);
-    Wizard::fly(&person);
-    person.fly();
+    println!("A baby dog is called a {}", Dog::baby_name());
+    println!("A baby dog is called a {}", <Dog as Animal>::baby_name());
 }
